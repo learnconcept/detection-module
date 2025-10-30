622f633dd90a6a998843fa9430a17445084812df
learnconcept <learnconcept.java@gmail.com>
2025-10-31 05:09:24 +0530

    First commit

---
+++ src/main/java/com/javaconcept/DetectionModuleApplication.java
@@ -0,0 +1,13 @@
+package com.javaconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectionModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectionModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaconcept/DetectionModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectionModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
